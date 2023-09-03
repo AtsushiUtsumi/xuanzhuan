cbae6738488616feb5cc8f81edf6256aa7e22684
AtsushiUtsumi <debtgx103@gmail.com>
2023-09-03 10:41:58 +0900

    仕様書出力コード作成を進める

--- xuanzhuan/excel/docs.py
+++ xuanzhuan/excel/docs.py
@@ -1,18 +1,26 @@
-
 from openpyxl import Workbook
 from openpyxl.styles import Alignment
 
-# 新しいワークブックを作成
-workbook = Workbook()
-sheet = workbook.active
+def create_docs(docs_name: str):
+    """
+    仕様書のエクセルファイルを出力します
 
-# セルに改行を含むテキストを設定
-text_with_newline = "行1\n行2\n行3"
-sheet['A1'] = text_with_newline
+    Args:
+        docs_name (string): 出力するファイルの名前(拡張子無し)
+    """
 
-# セル内のテキストを中央揃えで改行
-alignment = Alignment(wrap_text=True, vertical='center', horizontal='center')
-sheet['A1'].alignment = alignment
+    # 新しいワークブックを作成
+    workbook = Workbook()
+    sheet = workbook.active
 
-# ワークブックを保存
-workbook.save('cell_with_newline.xlsx')
+    # セルに改行を含むテキストを設定
+    text_with_newline = "行1\n行2\n行3"
+    sheet["A1"] = text_with_newline
+
+    # セル内のテキストを中央揃えで改行
+    alignment = Alignment(wrap_text=True, vertical="top", horizontal="left")
+    sheet["A1"].alignment = alignment
+
+    # ワークブックを保存
+    workbook.save(f'{docs_name}.xlsx')
+    return

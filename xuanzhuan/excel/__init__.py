12e0bf034ab01dcc85166c098bd13d955c76676c
AtsushiUtsumi <debtgx103@gmail.com>
2023-09-06 06:38:49 +0900

    「readme」的なものを「__init__.py」に追加すればいいのではと思った

--- xuanzhuan/excel/__init__.py
+++ xuanzhuan/excel/__init__.py
@@ -1,2 +1,7 @@
 from .docs import create_docs
 from .docs import Docs
+
+def readme():
+    print('ここでマニュアルを出せばいいのでは')
+    print('xuanzhuan内蔵の仕様書作成メソッド群です。出力する文字列の先頭docs.add(座標(0オリジン),"文字列")の形で入力することを目標にします座標とはシートオブジェクトなら行と列、行オブジェクトなら列番号、のように自身でセルを特定する座標')
+    return

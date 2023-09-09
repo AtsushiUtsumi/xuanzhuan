59051ecfeae86dc2356a08f01dc2201dd3bdf4bd
AtsushiUtsumi <debtgx103@gmail.com>
2023-09-09 14:30:22 +0900

    サンプルデータ総当たり作成メソッドを追加

--- xuanzhuan/db.py
+++ xuanzhuan/db.py
@@ -29,3 +29,39 @@ def write(query: str):
     conn.commit()
     conn.close()
     return
+
+# サンプルデータ作成メソッド
+
+def create_ll_from_sample_dict(params: dict[str, str]) -> list[list[str]]:
+    query = ''
+    ll = list()
+    for column_name, sample_value_list in params.items():
+        tmp = list()
+        for sample_value in sample_value_list:
+            tmp.append(f'{column_name}={sample_value}')
+        ll.append(tmp)
+    return ll
+
+# 亜種だけど、こっちの方が使いやすそう
+# 「カラム名:サンプル値のリスト」の辞書型を受け取り、総当たりを返す
+def create_ld_from_sample_dict(params: dict[str, str]) -> list[dict[str,str]]:
+    ret = list()
+    ll = create_ll_from_sample_dict(params)
+    ll = list_dot(ll)
+    print('以下の通り')
+    print(ll)
+    for l in ll:
+        tmp = dict()
+        for s in l:
+            tmp[s.split('=')[0]] = s.split('=')[1]
+        ret.append(tmp)
+    return ret
+
+from .utils.list_dot import list_dot
+
+def create_insert_sample_query(table_name: str, params: dict[str, str]) -> list[str]:
+    ret = []
+    ll = list_dot(create_ll_from_sample_dict(params))
+    for i in ll:
+        ret.append('INSERT ' + str(i))
+    return ret

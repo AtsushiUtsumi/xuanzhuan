1ae5574bc1cb28de2d65b6ac809bc123ff98835b
AtsushiUtsumi <debtgx103@gmail.com>
2023-10-01 23:55:02 +0900

    Thymeleafを追加

--- xuanzhuan/layer/presentation/spring.py
+++ xuanzhuan/layer/presentation/spring.py
@@ -16,7 +16,7 @@ class PresentationSpring(Presentation):
         self.project_root = f'{output_dir}/{project_name}'
         self.controller_root = f'{self.project_root}/src/main/java/com/example/{project_name}/controller'
         self.templates_root = f'{self.project_root}/src/main/resources/templates'
-        cmd = '(cd ' + output_dir + ') && (spring init -d=web --type gradle-project --build=gradle -n=' + project_name + ' ' + project_name + ')'
+        cmd = '(cd ' + output_dir + ') && (spring init -d=web,thymeleaf --type gradle-project --build=gradle -n=' + project_name + ' ' + project_name + ')'
         # 各種ディレクトリ作成
         subprocess.run(cmd, shell=True, capture_output=True, text=True)
         os.mkdir(self.controller_root)
@@ -34,9 +34,11 @@ class PresentationSpring(Presentation):
         lower_camel = xz.CaseConverter(screen_name).to_lower_camel_case()
         controller_file_name = f'{self.controller_root}/{upper_camel}Controller.java'
         form_file_name = f'{self.controller_root}/{upper_camel}Form.java'
-        html_file_name = f'{self.templates_root}/{lower_camel}.html'
         package = f'com.example.{self._project_name}.controller'
         xz.create_concrete_from_params(f'{xz.__templates_dir__}/Controller.java.j2', {'name': screen_name, 'package': package}, controller_file_name)
         xz.create_concrete_from_params(f'{xz.__templates_dir__}/Form.java.j2', {'name': screen_name, 'package': package}, form_file_name)
-        xz.create_concrete_from_params(f'{xz.__templates_dir__}/page.html.j2', {'name': screen_name}, html_file_name)
+        xz.create_concrete_from_params(f'{xz.__templates_dir__}/list.html.j2', {'name': screen_name}, f'{self.templates_root}/{lower_camel}List.html')
+        xz.create_concrete_from_params(f'{xz.__templates_dir__}/detail.html.j2', {'name': screen_name}, f'{self.templates_root}/{lower_camel}Detail.html')
+        xz.create_concrete_from_params(f'{xz.__templates_dir__}/edit.html.j2', {'name': screen_name}, f'{self.templates_root}/{lower_camel}Edit.html')
+        xz.create_concrete_from_params(f'{xz.__templates_dir__}/register.html.j2', {'name': screen_name}, f'{self.templates_root}/{lower_camel}Register.html')
         return

c0da3968902ae4e20fa60d57e6e97d71c26e1f03
AtsushiUtsumi <debtgx103@gmail.com>
2023-10-09 20:48:48 +0900

    テーブルの情報から空のファイルを作成してブラウザからアクセスできるように修正

--- xuanzhuan/layer/presentation/spring.py
+++ xuanzhuan/layer/presentation/spring.py
@@ -14,13 +14,21 @@ class PresentationSpring(Presentation):
     def __init__(self, output_dir, project_name):
         self._project_name = project_name
         self.project_root = f'{output_dir}/{project_name}'
+        self.repository_root = f'{self.project_root}/src/main/java/com/example/{project_name}/domain'
+        self.entity_root = f'{self.project_root}/src/main/java/com/example/{project_name}/entity'
+        self.service_root = f'{self.project_root}/src/main/java/com/example/{project_name}/servive'
         self.controller_root = f'{self.project_root}/src/main/java/com/example/{project_name}/controller'
+        self.form_root = f'{self.project_root}/src/main/java/com/example/{project_name}/form'
         self.templates_root = f'{self.project_root}/src/main/resources/templates'
         cmd = '(cd ' + output_dir + ') && (spring init -d=web,thymeleaf,postgresql,data-jpa --type gradle-project --build=gradle -n=' + project_name + ' ' + project_name + ')'
         # 各種ディレクトリ作成
         subprocess.run(cmd, shell=True, capture_output=True, text=True)
+        os.mkdir(self.form_root)
         os.mkdir(self.controller_root)
-        subprocess.run(f'(cd {self.project_root}) && (git init) && (git add --all) && (git commit -m プロジェクト作成)', shell=True, capture_output=True, text=True)
+        os.mkdir(self.service_root)
+        os.mkdir(self.repository_root)
+        os.mkdir(self.entity_root)
+        #subprocess.run(f'(cd {self.project_root}) && (git init) && (git add --all) && (git commit -m プロジェクト作成)', shell=True, capture_output=True, text=True)
         return
 
     def add_screen(self, screen_name):
@@ -37,19 +45,42 @@ class PresentationSpring(Presentation):
         xz.create_concrete_from_params(f'{xz.__templates_dir__}/detail.html.j2', {'name': screen_name}, f'{self.templates_root}/{lower_camel}Detail.html')
         xz.create_concrete_from_params(f'{xz.__templates_dir__}/edit.html.j2', {'name': screen_name}, f'{self.templates_root}/{lower_camel}Edit.html')
         xz.create_concrete_from_params(f'{xz.__templates_dir__}/register.html.j2', {'name': screen_name}, f'{self.templates_root}/{lower_camel}Register.html')
-        subprocess.run(f'(cd {self.project_root}) && (git add --all) && (git commit -m {screen_name}画面作成)', shell=True, capture_output=True, text=True)
+        #subprocess.run(f'(cd {self.project_root}) && (git add --all) && (git commit -m {screen_name}画面作成)', shell=True, capture_output=True, text=True)
         return
 
     def add_table(self, table):
         # Entity,Repository,生成クエリの追加
         # こういうのは「sugumi」に入れた方が良いんじゃないか
-        upper_camel = xz.CaseConverter(table).to_upper_camel_case()
-        lower_camel = xz.CaseConverter(table).to_lower_camel_case()
-        controller_file_name = f'{self.controller_root}/{upper_camel}Entity.java'
-        form_file_name = f'{self.controller_root}/{upper_camel}Repository.java'
-        package = f'com.example.{self._project_name}.controller'
-        xz.create_concrete_from_params(f'{xz.__templates_dir__}/add_table/Entity.java.j2', {'table': table, 'package': package}, controller_file_name)
-        xz.create_concrete_from_params(f'{xz.__templates_dir__}/add_table/Repository.java.j2', {'table': table, 'package': package}, form_file_name)
+        upper_camel = xz.CaseConverter(table.get('name')).to_upper_camel_case()
+        lower_camel = xz.CaseConverter(table.get('name')).to_lower_camel_case()
+
+        # htmlファイル名前設定
+        list_html_file_name = f'{self.templates_root}/{upper_camel}List.html'
+        register_html_file_name = f'{self.templates_root}/{upper_camel}Register.html'
+        edit_html_file_name = f'{self.templates_root}/{upper_camel}Edit.html'
+        # javaファイル名設定
+        controller_file_name = f'{self.controller_root}/{upper_camel}Controller.java'
+        service_file_name = f'{self.service_root}/{upper_camel}Service.java'
+        form_file_name = f'{self.controller_root}/{upper_camel}Form.java'
+        entity_file_name = f'{self.entity_root}/{upper_camel}.java'
+        repository_file_name = f'{self.repository_root}/{upper_camel}Repository.java'
+
+        package_root = f'com.example.{self._project_name}'
+        # HTML
+        xz.create_concrete_from_params(f'{xz.__templates_dir__}/add_table/list.html.j2', {'table': table}, list_html_file_name)
+        xz.create_concrete_from_params(f'{xz.__templates_dir__}/add_table/register.html.j2', {'table': table}, register_html_file_name)
+        xz.create_concrete_from_params(f'{xz.__templates_dir__}/add_table/edit.html.j2', {'table': table}, edit_html_file_name)
+        # Formクラス
+        xz.create_concrete_from_params(f'{xz.__templates_dir__}/add_table/ListForm.java.j2', {'table': table, 'package': package_root+'.form'}, form_file_name)
+        xz.create_concrete_from_params(f'{xz.__templates_dir__}/add_table/DetailForm.java.j2', {'table': table, 'package': package_root+'.form'}, form_file_name)
+        # Controllerクラス
+        xz.create_concrete_from_params(f'{xz.__templates_dir__}/add_table/Controller.java.j2', {'table': table, 'package': package_root+'.controller'}, controller_file_name)
+        # Serviceクラス
+        xz.create_concrete_from_params(f'{xz.__templates_dir__}/add_table/Service.java.j2', {'table': table, 'package': package_root+'.service'}, service_file_name)
+        # Repositoryクラス
+        xz.create_concrete_from_params(f'{xz.__templates_dir__}/add_table/Entity.java.j2', {'table': table, 'package': package_root+'.domain'}, entity_file_name)
+        xz.create_concrete_from_params(f'{xz.__templates_dir__}/add_table/Repository.java.j2', {'table': table, 'package': package_root+'.domain'}, repository_file_name)
+        # クエリ
         xz.create_concrete_from_params(f'{xz.__templates_dir__}/add_table/query.sql.j2', {'table': table}, f'{self.templates_root}/{lower_camel}.sql')
-        subprocess.run(f'(cd {self.project_root}) && (git add --all) && (git commit -m {table}テーブル作成)', shell=True, capture_output=True, text=True)
+        #subprocess.run(f'(cd {self.project_root}) && (git add --all) && (git commit -m {table}テーブル作成)', shell=True, capture_output=True, text=True)
         return
